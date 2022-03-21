5052bfb890ab69ae0b258b71077aafbb913bf1de
Lekhana S <55750780+lekhana30@users.noreply.github.com>
2022-03-21 20:59:40 +0530

    Add files via upload

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,44 @@
+﻿using Lib.Core.Model;
+using Lib.Service.PortfolioService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication11.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly IPortfolioService investorService;
+        public PortfolioController(IPortfolioService inService)
+        {
+            investorService = inService;
+        }
+        public async Task<IActionResult> List()
+        {
+            var list = await investorService.GetInvestor();
+            return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Portfolio model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                bool response = await investorService.SavePortfolio(model);
+
+                if (response)
+                {
+                    return RedirectToAction("List");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
